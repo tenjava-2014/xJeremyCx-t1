7f2461878d631303898f3f36649f624ce712f0e7
xJeremyCx <jeremy991119@gmail.com>
2014-07-12 17:59:19 +0800

    Last commit o_O

--- src/main/java/com/tenjava/entries/xJeremyCx/t1/TenJava.java
+++ src/main/java/com/tenjava/entries/xJeremyCx/t1/TenJava.java
@@ -11,7 +11,7 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TenJava extends JavaPlugin {
-    //wtf I just started ends in 2 hours 26 minutes 7 seconds
+    //wtf I just started. Ends in 2 hours 26 minutes 7 seconds
     //
 
     public static Plugin getPlugin() {
