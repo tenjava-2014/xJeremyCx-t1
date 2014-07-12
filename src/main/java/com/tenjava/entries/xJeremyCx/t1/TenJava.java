227505769d6b2d6fdae3a1e0ac1d2bde8be8bef6
xJeremyCx <jeremy991119@gmail.com>
2014-07-12 15:28:58 +0800

    Edited plugin.yml Added onEnable and onDiable

--- src/main/java/com/tenjava/entries/xJeremyCx/t1/TenJava.java
+++ src/main/java/com/tenjava/entries/xJeremyCx/t1/TenJava.java
@@ -2,4 +2,17 @@ package com.tenjava.entries.xJeremyCx.t1;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+
+}
