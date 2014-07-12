9f52534ba0d3bd2b66df98d5c487c457f0eca8da
xJeremyCx <jeremy991119@gmail.com>
2014-07-12 17:36:00 +0800

    Fixed lore not showing

--- src/main/java/com/tenjava/entries/xJeremyCx/t1/ItemsFactory.java
+++ src/main/java/com/tenjava/entries/xJeremyCx/t1/ItemsFactory.java
@@ -11,36 +11,36 @@ import java.util.List;
 public class ItemsFactory {
     //Create items
 
-    public static ItemStack getWarAxe()
-    {
+    public static ItemStack getWarAxe() {
         ItemStack i = new ItemStack(Material.IRON_AXE);
         ItemMeta im = i.getItemMeta();
         im.setDisplayName(ChatColor.GREEN + "WarAxe");
         List<String> lore = new ArrayList<String>();
         lore.add(ChatColor.RED + "Rocket target to the sky when hit!");
+        im.setLore(lore);
         i.setItemMeta(im);
         return i;
     }
 
-    public static ItemStack getMultiShoots()
-    {
+    public static ItemStack getMultiShoots() {
         ItemStack i = new ItemStack(Material.BOW);
         ItemMeta im = i.getItemMeta();
         im.setDisplayName(ChatColor.GREEN + "MultiShoots");
         List<String> lore = new ArrayList<String>();
         lore.add(ChatColor.RED + "Target and click to shoot!");
+        im.setLore(lore);
         i.setItemMeta(im);
         return i;
     }
 
-    public static ItemStack getEnderStick()
-    {
+    public static ItemStack getEnderStick() {
         ItemStack i = new ItemStack(Material.STICK);
         ItemMeta im = i.getItemMeta();
         im.setDisplayName(ChatColor.GREEN + "EnderSticks");
         List<String> lore = new ArrayList<String>();
         lore.add(ChatColor.RED + "Click a player to use it!");
         lore.add(ChatColor.DARK_RED + "Causing really high damage!");
+        im.setLore(lore);
         i.setItemMeta(im);
         return i;
     }
